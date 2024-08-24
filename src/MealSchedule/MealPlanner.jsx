eec1240a7286bbf0a9db888833d6be41260e9af0
sophiastudent123 <125307995+sophiastudent123@users.noreply.github.com>
2024-08-24 11:30:42 +0800

    code

--- src/MealSchedule/MealPlanner.jsx
+++ src/MealSchedule/MealPlanner.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Planner.css";
 
 const MealPlanner = () => {
+  // document elements
+  const itemClick = useRef();
+  const dropdownMenu = useRef();
+
+  const itemClicked = () => {
+    if (dropdownMenu.current.style.display==='none'){
+    dropdownMenu.current.style.display = 'block'
+    }
+    else {
+      dropdownMenu.current.style.display = 'none'
+    }
+  }
   return (
     <div className="planner-container">
       <h1>Meal Planner Scheducle</h1>
       <div className="add-item-container">
-        <a>Add Item</a>
         <div className="add-item-menu">
+        <a  onClick={itemClicked}ref ={itemClick}>Add Item</a>
+        <div ref={dropdownMenu}className="dropdown-menu">
           <h2>Add Item:</h2>
           <h3>Title:</h3> <input type="text"></input>
           <h3>Ingredients:</h3> <button>Add Ingredients</button>
+          </div>
         </div>
       </div>
-
+      <div className="planner-week">
       <div className="weekday">
         <h3>Monday</h3>
         <h3>Tuesday</h3>
@@ -44,6 +58,7 @@ const MealPlanner = () => {
       <div className="food-list-sunday">
         <ul></ul>
       </div>
+    </div>
     </div>
   );
 };
